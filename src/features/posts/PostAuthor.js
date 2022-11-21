20d255e3376d2a06d3530ce45aa9249466a5fba6
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-11-21 15:39:28 -0500

    created component to show new PostAuthor.

---
+++ src/features/posts/PostAuthor.js
@@ -0,0 +1,11 @@
+import { useSelector } from 'react-redux';
+import { getAllUsers } from '../users/usersSlice';
+
+const PostAuthor = ({ userId }) => {
+  const users = useSelector(getAllUsers);
+  const author = users.find((user) => user.id === userId);
+  console.log([userId, author]);
+  return <span>{author ? author.name : 'Unknown author'}</span>;
+};
+
+export default PostAuthor;
--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import PostAuthor from './PostAuthor';
 import { getAllPosts } from './postSlice';
 
 export const PostsList = () => {
@@ -9,6 +10,7 @@ export const PostsList = () => {
     <article className='post-excerpt' key={post.id}>
       <h3>{post.title}</h3>
       <p className='post-content'>{post.content.substring(0, 100)}</p>
+      <PostAuthor userId={post.userId} />
     </article>
   ));
 
