35d70377e66e814e71aea3f5120440079a918c70
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-11-21 13:06:04 -0500

    using post slice in app, the party is starting

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,14 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-    id: '1', title:'Learning redux Toolkit', content: 'I have heard good things...',
-    id: '1', title:'Slices...', content: 'The more I say slice, the more I want pizza...',
-}
+const initialState = [
+  {
+    id: '1',
+    title: 'Learning redux Toolkit',
+    content: 'I have heard good things...',
+  },
+  {
+    id: '1',
+    title: 'Slices...',
+    content: 'The more I say slice, the more I want pizza...',
+  },
+];
 
-export const postSlice = configureStore({
-    name:'posts',
-    initialState,
-    reducer: {},
-})
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducer: {},
+});
 
-export default postSlice.reducer;
+//creating selectors
+export const getAllPosts = (state) => state.posts;
+
+export default postsSlice.reducer;
