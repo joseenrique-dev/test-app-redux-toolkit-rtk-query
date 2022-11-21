a081cd2b2170f338311b8ca146ab309ed0e29743
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-11-21 12:31:07 -0500

    created postList component.

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import postReducer from '../features/posts/postSlice;'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    posts: postReducer
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
