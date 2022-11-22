dfa344c8b2b1e1013ee4dbda35bd5f05490d2748
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-11-21 22:45:55 -0500

    using extraReducers to handle the different etaps of the loading posts

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -4,7 +4,6 @@ import { getAllUsers } from '../users/usersSlice';
 const PostAuthor = ({ userId }) => {
   const users = useSelector(getAllUsers);
   const author = users.find((user) => user.id === userId);
-  console.log([userId, author]);
   return <span>by {author ? author.name : 'Unknown author'}</span>;
 };
 
