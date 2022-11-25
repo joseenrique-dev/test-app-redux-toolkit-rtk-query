6394df92cb083a09383555c176d8b666305b5ed1
José Enrique López Obeso <joseenrique.dev@gmail.com>
2022-11-24 22:57:38 -0500

    changed card style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,10 +7,18 @@ const Layout = () => {
   return (
     <>
       <Header />
-      <div className='bg-gray-300'>
+      <div className=''>
         <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
           <div className='mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32'>
-            <h2 className='text-2xl font-bold text-gray-900'>Collections</h2>
+            <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+              Robots Posts
+            </h2>
+            <p className='mt-4 text-gray-500'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde et
+              architecto aspernatur incidunt voluptas! Nemo placeat amet
+              assumenda, quia est beatae maxime quo corrupti repellendus velit,
+              aliquid qui modi excepturi.
+            </p>
 
             <div className='mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0'>
               <Outlet />
--- src/features/posts/PostsExcerpt.js
+++ src/features/posts/PostsExcerpt.js
@@ -3,50 +3,34 @@ import ReactionButtons from './ReactionButton';
 import PostAuthor from './PostAuthor';
 import TimeAgo from './TimeAgo';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getAllUsers } from '../users/usersSlice';
 
 const PostsExcerpt = ({ post }) => {
+  const users = useSelector(getAllUsers);
+  const author = users.find((user) => user.id === post.userId);
   return (
-    <div className='group relative'>
-      <div className='relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1'>
-        <img
-          src='https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg'
-          alt='Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.'
-          className='h-full w-full object-cover object-center'
-        />
-      </div>
-      <h3 className='mt-6 text-sm text-gray-500'>
-        <a href='#'>
-          <span className='absolute inset-0'></span>
-          {post.title}
-        </a>
-      </h3>
-      <p className='text-base font-semibold text-gray-900'>
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-        <ReactionButtons post={post} />
-      </p>
+    <div className='group relative p-3'>
+      <Link to={`post/${post.id}`}>
+        <div class='max-w-sm rounded overflow-hidden shadow-lg bg-gray-300'>
+          <img
+            src={`https://robohash.org/${author.name} ${post.id}`}
+            alt='Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.'
+            className='h-full w-full object-cover object-center'
+          />
+          <div class='px-6 py-4'>
+            <div class='font-bold text-xl mb-2'>Robot: {author.name}</div>
+            <p class='text-gray-700 text-base'>{post.title}</p>
+          </div>
+          <div class='px-6 pt-4 pb-2'>
+            <PostAuthor userId={post.userId} />
+            <TimeAgo timestamp={post.date} />
+            <ReactionButtons post={post} />
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
 
 export default PostsExcerpt;
-
-// <article className='post-excerpt'>
-//       <h3>{post.title}</h3>
-//       <p className='post-content'>{post.body.substring(0, 75)}...</p>
-//       <p className='postCredit'>
-//         <PostAuthor userId={post.userId} />
-//         <TimeAgo timestamp={post.date} />
-//         <ReactionButtons post={post} />
-//       </p>
-//       <Link to={`post/${post.id}`}>
-//         <p
-//           style={{
-//             color: 'white',
-//             fontWeight: 'bold',
-//           }}
-//         >
-//           View
-//         </p>
-//       </Link>
-//     </article>
